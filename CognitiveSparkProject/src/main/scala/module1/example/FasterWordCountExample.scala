8b5f46a079669066af04c6e0e80a9febc26a0232
shashi <sh.shankar1@gmail.com>
2017-12-28 21:49:30 +0530

    Word Count Example in Spark

--- CognitiveSparkProject/src/main/scala/module1/example/FasterWordCountExample.scala
+++ CognitiveSparkProject/src/main/scala/module1/example/FasterWordCountExample.scala
@@ -3,8 +3,7 @@ package module1.example
 import org.apache.spark.{SparkConf, SparkContext}
 import util.Files
 
-
-object WordCountExample {
+object FasterWordCountExample {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("Cognitive Classes").setMaster("local")
     val sparkContext = new SparkContext("local", "Word Count", conf)
@@ -12,9 +11,9 @@ object WordCountExample {
     val input = sparkContext.textFile("data/shakespeare/all-shakespeare.txt")
 
     val wc = input.map(_.toLowerCase)
-                .flatMap(text => text.split("""\W+"""))
-                .groupBy(word => word)
-                .mapValues(group => group.size)
+      .flatMap(text => text.split("""\W+"""))
+      .map(word => (word, 1))
+      .reduceByKey((n1, n2) => n1 + n2)
 
     val output = "output/word-count"
     Files.rmrf(output)
