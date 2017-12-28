0eefb0061cb85067630a89ac2f857d72c5161eac
shashi <sh.shankar1@gmail.com>
2017-12-28 17:35:23 +0530

    more examples

--- CognitiveScalaProject/src/main/scala/series1/module5/example/HandlingFutureExample.scala
+++ CognitiveScalaProject/src/main/scala/series1/module5/example/HandlingFutureExample.scala
@@ -2,22 +2,22 @@ package series1.module5.example
 
 import java.util.concurrent.ForkJoinPool
 
-import scala.concurrent.ExecutionContext
-import scala.concurrent.Future
+import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 
 object HandlingFutureExample {
   def main(args: Array[String]): Unit = {
     implicit val ec: ExecutionContext = ExecutionContext.fromExecutor(new ForkJoinPool())
-    implicit val timeout = 1 second
+    implicit val timeout = 3 second
 
     val f:Future[Unit] = Future{
       Thread.sleep(2000l)
       println("hello")
-    }
+      //throw new Exception()
+    }(ec)
 
-    ec.
+    Await.result(f,timeout)
 
     f.onComplete{
       case Success(())=>println("completed")
